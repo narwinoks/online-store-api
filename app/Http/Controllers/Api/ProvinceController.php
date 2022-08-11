8a35c98f32af07c51b0d3e65b99a91e7c271265f
winarno <narnowin195@gmail.com>
2022-08-11 22:08:06 +0700

    seeder

---
+++ app/Http/Controllers/Api/ProvinceController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\BaseController;
+use Illuminate\Http\Request;
+
+class ProvinceController extends BaseController
+{
+    public function getData(){
+        
+    }
+}
